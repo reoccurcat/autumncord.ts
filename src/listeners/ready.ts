d2ff8bdd95ffb7f4b880260bdae6bfd8a88181a3
reoccurcat <reoccurcat@gmail.com>
2022-01-10 16:02:41 -0600

    ok here

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -2,8 +2,13 @@ import { Listener } from '@sapphire/framework';
 import type { Client } from 'discord.js';
 
 export class ReadyListener extends Listener {
-  public run(client: Client) {
+  public async run(client: Client) {
     const { username, id } = client.user!;
     this.container.logger.info(`Successfully logged in as ${username} (${id})`);
+    if (process.env.test === "true") {
+      await new Promise(f => setTimeout(f, 10000));
+      console.log("Successfully ran for 10 seconds. Exiting...")
+      return client.destroy()
+    }
   }
 }
