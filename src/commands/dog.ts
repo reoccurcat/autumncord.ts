5e566d21afed2146ee42293fa2628918df540c06
reoccurcat <reoccurcat@gmail.com>
2022-01-14 15:06:18 -0600

    cat and dog picture commands finalized

--- src/commands/dog.ts
+++ src/commands/dog.ts
@@ -1,14 +1,13 @@
 import { Args, Command } from '@sapphire/framework';
 import axios from 'axios';
-import { BufferResolvable, Message, MessageActionRow, MessageAttachment, MessageButton, MessageEmbed } from 'discord.js';
-import type { Stream } from 'stream';
+import { Message, MessageActionRow, MessageAttachment, MessageButton, MessageEmbed } from 'discord.js';
 
-export class CatCommand extends Command {
+export class DogCommand extends Command {
   public constructor(context: Command.Context, options: Command.Options) {
     super(context, {
       ...options,
-      name: 'cat',
-      description: 'get a cat picture',
+      name: 'dog',
+      description: 'get a dog picture',
     });
   }
   public async messageRun(message: Message, args: Args) {
@@ -16,32 +15,31 @@ export class CatCommand extends Command {
     const text = await args.pick('string').catch(() => "none")
     let customIdImage = Math.floor(Math.random() * 10000)
     let customIdDisable = Math.floor(Math.random() * 10000)
-    let url: string
-    var fetchedurl: BufferResolvable | Stream 
+    let url: string 
     const filter = (i: { customId: string; user: { id: any; }; }) => i.customId === String(customIdImage) || i.customId === String(customIdDisable)
     const collector = message.channel.createMessageComponentCollector({ filter, time: 30000 });
-    if (text !== "none") {url = `https://cataas.com/cat/${text}`} else {url = 'https://cataas.com/cat'}
-    fetchedurl = await (await axios.get(url, { responseType: 'arraybuffer' })).data
-    let attachment = new MessageAttachment(fetchedurl, 'cat.png');
+    if (text !== "none") {url = `https://dog.ceo/api/breed/${text}/images/random`} else {url = 'https://dog.ceo/api/breeds/image/random'}
+    let fetchedurl = JSON.parse(await (await axios.get(url, { responseType: 'arraybuffer' })).data)
+    let attachment = new MessageAttachment(fetchedurl.message, 'dog.png');
     let embed = new MessageEmbed()
         .setColor('#5a1da1')
-        .setImage('attachment://cat.png')
-    const catimage = new MessageActionRow()
+        .setImage('attachment://dog.png')
+    const dogimage = new MessageActionRow()
         .addComponents(
             new MessageButton()
                 .setCustomId(String(customIdImage))
-                .setLabel('Another cat (image)')
+                .setLabel('Another dog')
                 .setStyle('PRIMARY'),
             new MessageButton()
                 .setCustomId(String(customIdDisable))
                 .setLabel('End interaction')
                 .setStyle('SECONDARY'),
     );
-    const catdisabled = new MessageActionRow()
+    const dogdisabled = new MessageActionRow()
         .addComponents(
             new MessageButton()
                 .setCustomId(String(customIdImage))
-                .setLabel('Another cat (image)')
+                .setLabel('Another dog')
                 .setStyle('SECONDARY')
                 .setDisabled(true),
             new MessageButton()
@@ -50,20 +48,20 @@ export class CatCommand extends Command {
                 .setStyle('SECONDARY')
                 .setDisabled(true)
     );
-    await msg.edit({content: "Meow!", embeds: [embed], files: [attachment], components: [catimage]})
+    await msg.edit({content: "Woof!", embeds: [embed], files: [attachment], components: [dogimage]})
     collector.on('collect', async i => {
       i.deferUpdate();
       if (i.customId === String(customIdImage)) { 
-        if (text !== "none") {url = `https://cataas.com/cat/${text}`} else {url = 'https://cataas.com/cat'}
-        fetchedurl = await (await axios.get(url, { responseType: 'arraybuffer' })).data
-        attachment = new MessageAttachment(fetchedurl, 'cat.png');
-        await msg.edit({embeds: [embed], files: [attachment], components: [catimage]})
+        if (text !== "none") {url = `https://dog.ceo/api/breed/${text}/images/random`} else {url = 'https://dog.ceo/api/breeds/image/random'}
+        fetchedurl = JSON.parse(await (await axios.get(url, { responseType: 'arraybuffer' })).data)
+        attachment = new MessageAttachment(fetchedurl.message, 'dog.png');
+        await msg.edit({embeds: [embed], files: [attachment], components: [dogimage]})
       } else if (i.customId === String(customIdDisable)) {
-        await msg.edit({embeds: [embed], files: [attachment], components: [catdisabled]})
+        await msg.edit({embeds: [embed], files: [attachment], components: [dogdisabled]})
       }
     });
     collector.on('end', () => {
-      msg.edit({ embeds: [embed], components: [catdisabled] })
+      msg.edit({ embeds: [embed], components: [dogdisabled] })
   });
   }
 }
